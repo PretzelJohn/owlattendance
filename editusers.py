aad07698dd573f91ad40049a3793fe62c650c7c6
PretzelJohn <john.luke.hussey@gmail.com>
2021-10-16 03:42:07 -0400

    Update user editor

--- editusers.py
+++ editusers.py
@@ -4,16 +4,22 @@ from app.models import Users
 
 if __name__ == '__main__':
     if len(sys.argv) < 1: sys.exit(0)
-    if len(sys.argv) == 1:
+    if len(sys.argv) == 2 and sys.argv[1] == "list":
+        users = db.session.query(Users).order_by(Users.user_id.asc()).all()
+        print("List of current usernames:")
+        for user in users:
+            print("{:>5}: {}".format(user.user_id, user.username))
+    elif len(sys.argv) == 3 and sys.argv[1] == "del":
+        id = sys.argv[2]
+        db.session.query(Users).filter(Users.user_id == id).delete()
+        db.session.commit()
+    elif len(sys.argv) == 4 and sys.argv[1] == "add":
+        u = Users(username=sys.argv[2])
+        u.set_password(sys.argv[3])
+        db.session.add(u)
+        db.session.commit()
+    else:
         print("Usage:")
         print("  list")
         print("  add <username> <password>")
-        print("  del <username>")
-    elif len(sys.argv) == 2 and sys.argv[1] == "list":
-        
-    if(sys.argv[1] == "")
-
-    u = Users(username=sys.argv[1])
-    u.set_password(sys.argv[2])
-    db.session.add(u)
-    db.session.commit()
+        print("  del <user_id>")
